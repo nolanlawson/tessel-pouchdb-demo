a1a90183eda93466b556ceed2204ad7f5d937f71
Nolan Lawson <nolan.lawson@gmail.com>
2016-06-08 06:11:56 -0700

    remove unneeded files

--- test.js
+++
@@ -1,7 +0,0 @@
-var PouchDB = require('pouchdb');
-require('pouchdb/extras/websql');
-
-var db = new PouchDB('test.db', {adapter: 'websql'});
-db.post({}).then(function () {
-  console.log('done');
-}).catch(console.log);
--- websql-demo.js
+++
@@ -1,8 +0,0 @@
-/*
-var websql = require('websql');
-
-var db = websql('/mnt/sda2/tessel.db', 1, '', 1);
-db.transaction(function (tx) {
-  tx.executeSql('create table if not exists foo (bar);');
-});
-*/
